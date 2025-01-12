dc5efec46463ba6b7c80efcb95571a76184a89fb
ricardo2224 <gaofenl1@uci.edu>
2025-01-11 20:30:19 -0800

    Complete the game project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,54 @@ public class Player : MonoBehaviour
     [SerializeField] private int _numSeeds = 5; 
     [SerializeField] private PlantCountUI _plantCountUI;
 
-    private int _numSeedsLeft;
-    private int _numSeedsPlanted;
+    private int _numSeedsLeft = 5;
+    private int _numSeedsPlanted = 0;
 
     private void Start ()
     {
-        
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+     
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime); 
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+       
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
+
+     
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+          Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+
+            _numSeedsLeft = _numSeedsLeft - 1;
+            _numSeedsPlanted = _numSeedsPlanted + 1;
+
+            _plantCountUI.UpdateSeeds(_numSeedsLeft,_numSeedsPlanted);
+        }
+
     }
 }
